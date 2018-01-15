ec1cdb4378bc0072c4e580391bbd6e969a80fcda
Loryn Chen <loryn.chen@gmail.com>
2018-01-15 00:49:48 -0500

    Redesigned new site for Spring 2018

---
+++ JS/animations-script.js
@@ -0,0 +1,49 @@
+var animation1 = lottie.loadAnimation({
+    container: document.getElementById('bm-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../JSON/animation.json'
+})
+
+var animation2 = lottie.loadAnimation({
+    container: document.getElementById('bm-film'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../JSON/film.json'
+})
+
+
+var animation3 = lottie.loadAnimation({
+    container: document.getElementById('bm-creative'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../JSON/creative.json'
+})
+
+
+var animation4 = lottie.loadAnimation({
+    container: document.getElementById('bm-engaging'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../JSON/engaging.json'
+})
+
+var animation5 = lottie.loadAnimation({
+    container: document.getElementById('bm-vibrant'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../JSON/vibrant.json'
+})
+
+var animation6 = lottie.loadAnimation({
+    container: document.getElementById('bm-envisioning'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../JSON/envisioning.json'
+})
